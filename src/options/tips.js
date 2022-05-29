81068f4bec5b54314be194c2e5a8eda56040bcc9
evgo2017 <evgo2017@qq.com>
2022-05-30 01:47:04 +0800

    fix: 右侧图标在某些情况下缺失，为 fa-* 增加了类名前缀 custom，以和原本的 fa 区分，并 svg 上设置高宽

--- src/options/tips.js
+++ src/options/tips.js
@@ -8,22 +8,22 @@ export default {
     texts: ['哇，你终于回来了～']
   }],
   mouseover: [{
-    selector: '.vue-live2d-tool .fa-comment',
+    selector: '.vue-live2d-tool .custom-fa-comment',
     texts: ['猜猜我要说些什么？', '欣赏一些有意思的短句？']
   }, {
-    selector: '.vue-live2d-tool .fa-user-circle',
+    selector: '.vue-live2d-tool .custom-fa-user-circle',
     texts: ['想看看我的朋友们吗？']
   }, {
-    selector: '.vue-live2d-tool .fa-street-view',
+    selector: '.vue-live2d-tool .custom-fa-street-view',
     texts: ['变装！']
   }, {
-    selector: '.vue-live2d-tool .fa-camera-retro',
+    selector: '.vue-live2d-tool .custom-fa-camera-retro',
     texts: ['一起拍照留念？']
   }, {
-    selector: '.vue-live2d-tool .fa-info-circle',
+    selector: '.vue-live2d-tool .custom-fa-info-circle',
     texts: ['(＾Ｕ＾)ノ~ＹＯ']
   }, {
-    selector: '.vue-live2d-tool .fa-times',
+    selector: '.vue-live2d-tool .custom-fa-times',
     texts: ['就要说再见了吗？']
   }]
 }
